835cc3f69db1fe5fa4428aef03918d8bd7814a15
Evan Jenkins <evan@activelamp.com>
2017-07-26 17:01:26 -0700

    adding more configs

--- app/web/profiles/reactivelamp/modules/custom/reactivelamp_spa/src/Twig/ReactRenderer.php
+++ app/web/profiles/reactivelamp/modules/custom/reactivelamp_spa/src/Twig/ReactRenderer.php
@@ -1,27 +1,39 @@
 <?php
 
 namespace Drupal\reactivelamp_spa\Twig;
+use Drupal\Core\Site\Settings;
 use Limenius\ReactRenderer\Renderer\PhpExecJsReactRenderer;
 use Limenius\ReactRenderer\Twig\ReactRenderExtension;
+use Limenius\ReactRenderer\Context\SymfonyContextProvider;
+use Twig_Extension_StringLoader;
 
 class ReactRenderer extends \Twig_Extension
 {
   public function getName()
   {
-    return 'react_renderer';
+    return 'react_component';
   }
 
   public function getFunctions()
   {
-    global $base_url;
+    return [
+      new \Twig_SimpleFunction('react_renderer', [$this, "getReactRenderer"])
+    ];
+  }
 
-//    $environment = Settings::get('environment');
-//    $url = $environment === 'dev' ? 'http://localhost:3000/bundle.js' : $base_url;
-//    $renderer = new PhpExecJsReactRenderer($url);
-//    $ext = new ReactRenderExtension($renderer, 'both');
-//
-//    return [
-//      new
-//    ];
+  public function getReactRenderer()
+  {
+    global $base_url;
+    $environment = Settings::get('environment');
+    $requestStack = \Drupal::requestStack();
+    // SymfonyContextProvider provides information about the current request, such as hostname and path
+    // We need an instance of Symfony\Component\HttpFoundation\RequestStack to use it
+    $url = $environment === 'dev' ? 'http://localhost:3000/bundle.js' : $base_url . '/bundle.js';
+    $contextProvider = new SymfonyContextProvider($requestStack);
+    $renderer = new PhpExecJsReactRenderer($url, false, $contextProvider);
+    $ext = new ReactRenderExtension($renderer, $contextProvider, 'both');
+    return $ext;
+//    $twig->addExtension(new Twig_Extension_StringLoader());
+//    $twig->addExtension($ext);
   }
 }
